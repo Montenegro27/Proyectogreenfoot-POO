9bfa2bd1cafe181fae1424525d1e84f747875193
Montenegro27 <118623623+Montenegro27@users.noreply.github.com>
2023-07-23 23:21:16 -0600

    0.02
    Agregar que la capibara comiera

--- MascotaVirtual/Capi.java
+++ MascotaVirtual/Capi.java
@@ -8,12 +8,48 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Capi extends Actor
 {
-    /**
-     * Act - do whatever the Capi wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private boolean menuExtended = false;  // Variable para controlar si el menú está extendido o no
+    
     public void act()
     {
-        // Add your action code here.
+        // Verifica si el jugador presiona la tecla "c"
+        if (Greenfoot.isKeyDown("c"))
+        {
+            // Si el menú no está extendido, llama al método para extenderlo
+            if (!menuExtended)
+            {
+                showMenu();
+                menuExtended = true;
+            }
+            // Si el menú ya está extendido, llama al método para ocultarlo
+            else
+            {
+                hideMenu();
+                menuExtended = false;
+            }
+        }
+    }
+    
+    private void showMenu()
+    {
+        // Agrega todas las comidas al menú
+        World world = getWorld();
+        world.addObject(new Chocolate(), getX(), getY());
+        world.addObject(new Hamburguesa(), getX(), getY() + 50);
+        world.addObject(new Helado(), getX(), getY() + 100);
+        world.addObject(new Naranja(), getX(), getY() + 150);
+        world.addObject(new Pizza(), getX(), getY() + 200);
+    }
+    
+    private void hideMenu()
+    {
+        // Elimina todos los objetos de comida del menú
+        World world = getWorld();
+        world.removeObjects(world.getObjects(Comida.class));
+    }
+    public void recibirComida(Comida comida)
+    {
+        // Aquí puedes definir lo que sucede cuando la capibara recibe una comida
+        // Por ejemplo, puedes cambiar la imagen de la capibara o aumentar su puntaje
     }
 }
---
+++ MascotaVirtual/Hamburguesa.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hamburguesa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hamburguesa extends Comida
+{
+    /**
+     * Act - do whatever the Hamburguesa wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
