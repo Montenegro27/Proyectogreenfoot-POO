6109b83de56d6698c89b4221062e315c7cbfee39
Montenegro27 <118623623+Montenegro27@users.noreply.github.com>
2023-07-23 21:13:14 -0600

    0.01
    Actualización de capibara

--- MascotaVirtual/MyWorld.java
+++ MascotaVirtual/MyWorld.java
@@ -17,5 +17,18 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Capi capi = new Capi();
+        addObject(capi,35,54);
+        capi.setLocation(420,207);
+        capi.setLocation(411,279);
     }
 }
